e896c73bbb8ccc899bb105063b716e00bc5a4b31
Flávia Penim <flavia.penim@ovoenergy.com>
2020-09-22 15:22:56 +0100

    Move tests into unit folder. location_test (wip)

---
+++ test/unit/location_test.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'package:test/test.dart';
+
+void main() {
+  test("test location deserialization", () {
+
+    const locationJSON =
+    '''
+    {
+      "name": "Arashiyama Bamboo Grove",
+      "url": "https://cdn-images-1.medium.com/max/2000/1*vdJuSUKWl_SA9Lp-32ebnA.jpeg",
+      "facts": [
+        {
+        "title": "Summary",
+        "text": "This bamboo grove is on the outskirts of Kyoto."
+        }
+      ]
+    }
+    ''';
+
+    final locationMap = json.decode(locationJSON) as Map<String, dynamic>;
+  });
+}
